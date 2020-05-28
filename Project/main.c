e666019eadf91ed5bbc2e8f012f8a6efb12b58c4
Soli99 <pi.solimeno@studenti.unina.it>
2020-05-28 16:41:40 +0200

    Login e registra

--- Project/main.c
+++ Project/main.c
@@ -1,8 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void menu();
+
 int main()
 {
-    printf("Hello world!\n");
+    menu();
     return 0;
+}
+
+void menu(B){
+
+    int scelta=-1;
+    do{
+        printf("\nSelezionare un operazione");
+        printf("\n\t\t\t 1.Login");
+        printf("\n\t\t\t 2.Registrati");
+        printf("\n\t\t\t 0.Esci");
+
+
+        printf("\n\n\n\t\t\t Enter choice => ");
+        scanf("%d",&scelta);
+
+        switch(scelta){
+
+        case 0:
+            exit(0);
+
+        default:
+            printf("Scelta non valida");
+
+        }
+
+    }while(scelta!=-1);
 }
